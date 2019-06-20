eb10e66650d601fdd89ee08fe4b435ef756f1180
Pedro Gabriel Leal Jimenez <gaboleal123@gmail.com>
2019-06-19 21:55:04 -0400

    nueva funcion en UserController

--- apirestcrud/app/Http/Controllers/UserController.php
+++ apirestcrud/app/Http/Controllers/UserController.php
@@ -53,4 +53,21 @@ class UserController extends Controller
         }
         return response()->json(compact('token'));
     }
+
+
+    public function getAuthentificatedUser(){
+        try{
+            if(!$user == JWTAuth::parseToken()->authenticate()){
+                return response()->json(['user_not_found'],404);
+            }
+        }catch(Tymon\JWTAuth\Exception\TokenExpiredException $e){
+                return response()->json(['token_expired'], $e->getStatusCode());
+        }catch(Tymon\JWTAuth\Exception\TokenInvalidException $e){
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        }catch(Tymon\JWTAuth\Exception\JWTException $e){
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
+    }
 }
